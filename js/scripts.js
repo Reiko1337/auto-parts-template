5e5fd44a4a2f1d0fff6a66614fe97c59cb8f0071
Reiko1337 <kurets11@gmail.com>
2021-04-12 17:09:06 +0300

    Version 1.0

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+const spoiler = document.querySelectorAll('.footer__item-title')
+
+for (let i = 0; i < spoiler.length; i++) {
+    spoiler[i].addEventListener("click", function () {
+        this.nextElementSibling.classList.toggle('spoiler-active')
+    });
+}
+
+$(document).ready(function() {
+    $('.car-brand, .model, .spare-part').select2({
+        placeholder: "Марка автомобиля",
+        maximumSelectionLength: 2,
+        language: "ru",
+        width: null
+    });
+    $('.model').select2({
+        placeholder: "Модель автомобиля",
+        maximumSelectionLength: 2,
+        language: "ru",
+        width: null
+    });
+    $('.spare-part').select2({
+        placeholder: "Название запчасти",
+        maximumSelectionLength: 2,
+        language: "ru",
+        width: null
+    });
+});
+
+lightbox.option({
+    'disableScrolling': true
+})
+
